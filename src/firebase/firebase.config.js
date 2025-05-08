19da069f86549660b2627c8949a56cf58bbe7c03
Rahul Dhar Roni <48157422+RahulDharRoni@users.noreply.github.com>
2025-05-08 14:06:02 -0400

    Reservation UI Wareframe Design

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC-qv44L8vQbWNhF534rCLTvCd4mclr7nc",
+  authDomain: "reservation-5c1ad.firebaseapp.com",
+  projectId: "reservation-5c1ad",
+  storageBucket: "reservation-5c1ad.firebasestorage.app",
+  messagingSenderId: "818889345321",
+  appId: "1:818889345321:web:80ce218a2044f437874f38",
+};
+
+export const app = initializeApp(firebaseConfig);
