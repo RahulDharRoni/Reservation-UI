af46886475ecf88d1baec0dec7e821fe5decb84b
Rahul Dhar Roni <48157422+RahulDharRoni@users.noreply.github.com>
2025-05-09 01:24:35 -0400

    login and signup page build

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC-qv44L8vQbWNhF534rCLTvCd4mclr7nc",
-  authDomain: "reservation-5c1ad.firebaseapp.com",
-  projectId: "reservation-5c1ad",
-  storageBucket: "reservation-5c1ad.firebasestorage.app",
-  messagingSenderId: "818889345321",
-  appId: "1:818889345321:web:80ce218a2044f437874f38",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 export const app = initializeApp(firebaseConfig);
